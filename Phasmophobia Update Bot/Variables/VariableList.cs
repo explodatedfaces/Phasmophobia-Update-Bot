36f3ddf1146bae9c2c09ece1c2cbb9e10fa637f6
ExplodatedFaces <spillanej09@gmail.com>
2022-09-27 03:24:30 -0400

    Remove unneeded using statements

--- Phasmophobia Update Bot/Variables/VariableList.cs
+++ Phasmophobia Update Bot/Variables/VariableList.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Phasmophobia_Update_Bot.Variables
+﻿namespace Phasmophobia_Update_Bot.Variables
 {
     internal class VariableList
     {
